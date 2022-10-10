94793387320f8e44db459e58663cfe51d7ebadaa
deepthedeata <98915818+deepthedeata@users.noreply.github.com>
2022-10-10 10:40:23 +0530

    Update Diabetes_Prediction_web_app.py

--- Diabetes_Prediction_web_app.py
+++ Diabetes_Prediction_web_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 #loading the model
 
-loaded_model = pickle.load(open('C:/Users/pr&d02/Desktop/diabetes/trained_model.sav','rb'))
+loaded_model = pickle.load(open('diabetes/trained_model.sav','rb'))
 
 #creating a function for prediction
 
